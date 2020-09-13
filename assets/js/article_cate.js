4e0d92011162e1762abc5b9e45d3dc36086b6444
guangrui <15822171057@163.com>
2020-09-13 15:33:08 +0800

    编辑功能完成

--- assets/js/article_cate.js
+++ assets/js/article_cate.js
@@ -1,5 +1,6 @@
 $(function () {
     var layer = layui.layer
+    var form = layui.form
     initArtCateList()
 
 
@@ -43,6 +44,48 @@ $(function () {
                 layer.msg('新增文章分类成功！')
                 // 关闭弹出层
                 layer.close(indexAdd)
+            }
+        })
+    })
+
+
+    // 设置编辑的点击事件(事件委派)
+    var indexEdit =null
+    $('tbody').on('click', '.btn-edit', function () {
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '300px'],
+            title: '修改文章分类',
+            content:$('#dialog-edit').html()
+        });  
+
+        // 根据id值发起获取文章请求数据,并填充到表单
+        // 1.获取id
+        var id = $(this).attr('data-id')
+        // 2.ajax 发起请求获取对应数据
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/'+id,
+            success: function (res) {
+                form.val('form-edit',res.data)
+            }
+        })
+
+    })
+    // 修改按钮点击事件,更新文章数据
+    $('body').on('submit', '#form-edit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !==0) {
+                    return layer.msg('更新分类信息失败！')
+                }
+                layer.msg('更新分类信息成功！')
+                layer.close(indexEdit)
+                initArtCateList()
             }
         })
     })
