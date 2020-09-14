fcaa837faf780fc42d0331ff0caf58bc867121b1
guangrui <15822171057@163.com>
2020-09-14 15:18:50 +0800

    发布文章的ajax请求发布文章完成

--- assets/js/article_pub.js
+++ assets/js/article_pub.js
@@ -35,6 +35,84 @@ $(function () {
     // 3. 初始化裁剪区域
     $image.cropper(options)
 
+    // 为选择按钮设置点击事件
+    $('#btnChooseImage').on('click', function () {
+        $('#coverFile').click()
+    })
+
+    // 监听coverFile的change事件,获取用户选择的文件
+    $('#coverFile').on('change', function (e) {
+        // console.log(e);
+        // 获取文件的列表数组
+        var files = e.target.files
+        // 判断用户是否选择文件
+        if (files,length===0) {
+            return
+        }
+        // 根据文件,创建对应的URL地址
+        var newImgURL = URL.createObjectURL(files[0])
+        // 为裁剪区域重新设置图片
+        $image
+           .cropper('destroy')      // 销毁旧的裁剪区域
+           .attr('src', newImgURL)  // 重新设置图片路径
+           .cropper(options)        // 重新初始化裁剪区域
+    })
+
+    // 定义文章的发布状态
+    var art_state = '已发布'
+
+    // 为存为草稿按钮,绑定点击处理函数
+    $('#btnSave2').on('click', function () {
+        art_state = '草稿'
+    })
+
+    // 为表单绑定submit提交事件
+    $('#form-pub').on('submit', function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+        // 基于form表单,快速创建一个FormDate
+        var fd = new FormData($(this)[0])
+        // 将文章的发布状态存到fd里
+        fd.append('state', art_state)
+        
+        // fd.forEach(function (v, k) {
+        //     console.log(k,v);
+        // })
+
+        $image
+            .cropper('getCroppedCanvas', {
+              // 创建一个 Canvas 画布
+              width: 400,
+              height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容,转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 将文件对象存储到fd里
+                fd.append('cover_img', blob)
+                
+                // 发起ajax请求发布文章
+                publishArticle(fd)
+            })
+    })
+
+    // 发起Ajax请求实现发布文章的功能
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.status !==0) {
+                    return layer.msg('发布文章失败！')
+                }
+                layer.msg('发布文章成功！')
+                location.href = '/article/article_list.html'
+            }
+        })
+    }
 
 
 })
